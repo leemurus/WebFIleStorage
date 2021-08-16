034a54a0abb36195d6df10d5742535cd9a9b5ced
Anton Sidarevich <asidarevich@webim-team.ru>
2021-08-16 21:32:12 +0300

    Minor fixes

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ import os
 
 from flask import Flask
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from config import config
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 login_manager = LoginManager()
 
@@ -16,6 +18,7 @@ def create_app(config_name='default'):
     app.config.from_object(config[config_name])
 
     db.init_app(app)
+    migrate.init_app(app)
     login_manager.init_app(app)
 
     # Create folder for store
--- config.py
+++ config.py
@@ -18,6 +18,7 @@ class DevelopmentConfig(Config):
 
 
 class ProductionConfig(Config):
+    DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
 
